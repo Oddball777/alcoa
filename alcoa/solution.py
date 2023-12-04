da9e9693dd5da277f4348d7354fc132ffac2b741
Gustave Coulombe <magikgus@gmail.com>
2023-12-04 14:11:38 -0500

    Ajout de la classe Substance, ajout du contrôle des time steps d'animations

--- alcoa/solution.py
+++ alcoa/solution.py
@@ -1,9 +1,27 @@
+from calendar import c
 from dataclasses import dataclass, field
 
 import matplotlib.pyplot as plt
 import numpy as np
 from findiff import PDE, BoundaryConditions, FinDiff
 from matplotlib.animation import FuncAnimation
+from sympy import Subs
+
+
+@dataclass
+class Substance:
+    """
+    Classe pour définir les propriétés d'une substance.
+    """
+
+    temperature: float
+    densite: float
+    chaleur_specifique: float
+    conductivite_thermique: float
+
+    @property
+    def rho_cp(self):
+        return self.densite * self.chaleur_specifique
 
 
 @dataclass
@@ -22,12 +40,9 @@ class Garnissage:
     - temperature_initiale_garnissage: K
     """
 
-    # Constantes pour l'équation différentielle
-    rho_cp_f: float  # kg/m3.K
-    rho_cp_eq: float  # kg/m3.K
-    k_eq: float  # W/m.K
+    substance: Substance
+    ratio_volume_solide: float
     vitesse_fluide: float  # m/s
-    # Paramètres de simulation
     longueur_garnissage: float  # m
     resolution_temporelle: float  # s
     resolution_spatiale: float  # m
@@ -36,18 +51,18 @@ class Garnissage:
 
     def demarrer_phase(
         self,
-        temperature_fluide: float,
+        fluide: Substance,
         direction: int = 1,
         duree: float = 15,
     ):
         """
         Démarre une phase de simulation. Les paramètres sont:
-        - temperature_fluide: température du fluide entrant dans le garnissage (K)
+        - fluide: objet Substance représentant le fluide et ses propriétés
         - direction: direction du fluide (1 pour entrant à gauche, -1 pour entrant à droite)
         - duree: durée de la phase (s)
         """
         print(
-            f"Phase {len(self.phases)+1} en cours: température du gaz {temperature_fluide} K"
+            f"Phase {len(self.phases)+1} en cours: température du gaz {fluide.temperature} K"
         )
         if len(self.phases) == 0:
             n_points = int(self.longueur_garnissage / self.resolution_spatiale)
@@ -56,12 +71,12 @@ class Garnissage:
             )
         else:
             temperature_initiale = self.phases[-1][:, -1]
-        T = self._simuler_phase(
-            temperature_initiale, temperature_fluide, duree, direction
-        )
+        T = self._simuler_phase(temperature_initiale, fluide, duree, direction)
         self.phases.append(T)
 
-    def _simuler_phase(self, initial_condition, fluid_temperature, duree, direction):
+    def _simuler_phase(
+        self, initial_condition, fluide: Substance, duree: float, direction: int
+    ):
         # Définir le domaine spatial
         x_max = self.longueur_garnissage
         x_min = 0
@@ -80,24 +95,33 @@ class Garnissage:
         d2T_dx2 = FinDiff(0, dx, 2)  # deuxième dérivée p/r à x
         dT_dt = FinDiff(1, dt, 1)  # première dérivée p/r à t
 
+        rho_cp_eq = (
+            self.substance.rho_cp * self.ratio_volume_solide
+            + (1 - self.ratio_volume_solide) * fluide.rho_cp
+        )
+        k_eq = (
+            self.substance.conductivite_thermique * self.ratio_volume_solide
+            + (1 - self.ratio_volume_solide) * fluide.conductivite_thermique
+        )
+
         if direction == 1 or direction == -1:
             u = self.vitesse_fluide * direction
         else:
             raise ValueError("direction must be 1 or -1")
-        lhs = self.rho_cp_eq * dT_dt + self.rho_cp_f * u * dT_dx - self.k_eq * d2T_dx2
+        lhs = rho_cp_eq * dT_dt + fluide.rho_cp * u * dT_dx - k_eq * d2T_dx2
         rhs = np.zeros((n_points, n_t_points))
 
         # Définir les conditions aux limites
         bc = BoundaryConditions(shape=(n_points, n_t_points))
-        if direction == 1:
-            bc[0, :] = fluid_temperature  # Température d'entrée à gauche
-            bc[-1, :] = dT_dx, 0  # Dérivée nulle à la sortie à droite
-        elif direction == -1:
-            bc[0, :] = dT_dx, 0  # Dérivée nulle à la sortie à gauche
-            bc[-1, :] = fluid_temperature  # Température d'entrée à droite
         # Condition initiale
         for x_point in range(n_points):
             bc[x_point, 0] = initial_condition[x_point]
+        if direction == 1:
+            bc[0, :] = fluide.temperature  # Température d'entrée à gauche
+            bc[-1, :] = dT_dx, 0  # Dérivée nulle à la sortie à droite
+        elif direction == -1:
+            bc[0, :] = dT_dx, 0  # Dérivée nulle à la sortie à gauche
+            bc[-1, :] = fluide.temperature  # Température d'entrée à droite
 
         pde = PDE(lhs, rhs, bc)
         T = pde.solve()
@@ -119,17 +143,19 @@ class Garnissage:
         T = np.concatenate(self.phases, axis=1)
         return T
 
-    def animer_ligne(self, nom_fichier="ligne"):
+    def animer_ligne(self, nom_fichier="ligne", step: int = 1):
         """
         Crée une animation du graphique de la température en fonction de la position.
         """
         print(f"Animation du graphique {nom_fichier}")
         T = self.get_resultats()
+        T_min = np.min(T)
+        T_max = np.max(T)
         fig, ax = plt.subplots()
         ax.set_xlabel("x (m)")
         ax.set_ylabel("Temperature (K)")
         ax.set_xlim(0, self.longueur_garnissage)
-        ax.set_ylim(0, 600)
+        ax.set_ylim(T_min, T_max)
         (line,) = ax.plot([], [], lw=2)
 
         time_text = ax.text(
@@ -146,16 +172,21 @@ class Garnissage:
             return (line,)
 
         def animate(i):
-            line.set_data(self.x, T[:, i])
-            time_text.set_text(f"t = {i*self.resolution_temporelle:.2f} s")
+            line.set_data(self.x, T[:, i * step])
+            time_text.set_text(f"t = {i*self.resolution_temporelle*step:.2f} s")
             return (line,)
 
         anim = FuncAnimation(
-            fig, animate, init_func=init, frames=T.shape[1], interval=20, blit=True
+            fig,
+            animate,
+            init_func=init,
+            frames=T.shape[1] // step,
+            interval=20,
+            blit=True,
         )
         anim.save(f"{nom_fichier}.mp4", writer="ffmpeg")
 
-    def animer_heatmap(self, nom_fichier="heatmap"):
+    def animer_heatmap(self, nom_fichier="heatmap", step: int = 1):
         """
         Crée une animation du graphique de la température en fonction de la position et du temps.
         """
@@ -164,6 +195,8 @@ class Garnissage:
         x_min, x_max = 0, self.longueur_garnissage
         y_min, y_max = 0, 3
         n_y_points = 20
+        T_min = np.min(T)
+        T_max = np.max(T)
 
         x = np.linspace(x_min, x_max, T.shape[0])
         y = np.linspace(y_min, y_max, n_y_points)
@@ -186,13 +219,13 @@ class Garnissage:
         )
 
         def animate2(i):
-            T_dup = np.tile(T[:, i], (n_y_points, 1)).T
+            T_dup = np.tile(T[:, i * step], (n_y_points, 1)).T
             heat_map.set_array(T_dup.T)
-            heat_map.set_clim(0, 600)
-            time_text.set_text(f"t = {i*self.resolution_temporelle:.2f} s")
+            heat_map.set_clim(T_min, T_max)
+            time_text.set_text(f"t = {i*self.resolution_temporelle*step:.2f} s")
             return (heat_map,)
 
-        anim = FuncAnimation(fig, animate2, frames=T.shape[1], interval=20)
+        anim = FuncAnimation(fig, animate2, frames=int(T.shape[1] / step), interval=20)
         anim.save(f"{nom_fichier}.mp4", writer="ffmpeg")
 
     def get_min_temperature_at_end(self):
@@ -204,70 +237,51 @@ class Garnissage:
         return np.max(T[:, -1])
 
 
-def get_proprietes_equivalentes(
-    ratio_volume_solide,
-    densite_solide,
-    densite_fluide,
-    chaleur_specifique_solide,
-    chaleur_specifique_fluide,
-    conductivite_thermique_solide,
-    conductivite_thermique_fluide,
-):
-    """
-    Retourne les propriétés équivalentes rho_cp_eq, rho_cp_f, k_eq à partir des propriétés des solides et des fluides
-    et du ratio entre le volume de solide et le volume de fluide.
-
-    Cette fonction assume que les propriétés des solides et des fluides sont exprimées dans les unités suivantes:
-    - densité: kg/m3
-    - chaleur spécifique: J/kg.K
-    - conductivité thermique: W/m.K
-    """
-    rho_cp_eq = (
-        ratio_volume_solide * densite_solide * chaleur_specifique_solide
-        + (1 - ratio_volume_solide) * densite_fluide * chaleur_specifique_fluide
-    )
-    k_eq = (
-        ratio_volume_solide * conductivite_thermique_solide
-        + (1 - ratio_volume_solide) * conductivite_thermique_fluide
-    )
-    rho_cp_f = densite_fluide * chaleur_specifique_fluide
-    return rho_cp_eq, rho_cp_f, k_eq
-
-
 if __name__ == "__main__":
-    # Définir les paramètres rho_cp_eq, rho_cp_f, k_eq et vitesse_fluide
-    rho_cp_eq, rho_cp_f, k_eq = get_proprietes_equivalentes(
-        ratio_volume_solide=0.5,
-        densite_solide=2600,
-        densite_fluide=1.225,
-        chaleur_specifique_solide=1000,
-        chaleur_specifique_fluide=1015,
-        conductivite_thermique_solide=2,
-        conductivite_thermique_fluide=0.035,
-    )
-    vitesse_fluide = 0.05  # vitesse en valeur absolue
+    debit = 0.9438949  # m3/s (valeur absolue)
+    A_conduite = 1  # m2
+    vitesse_fluide = debit / A_conduite  # m/s
 
-    # Réglage manuel pour les tests (à commenter si on utilise la fonction get_proprietes_equivalentes)
-    rho_cp_eq = 1
-    rho_cp_f = 1
-    k_eq = 1
+    co2 = Substance(
+        temperature=1090,
+        densite=0.4847,
+        chaleur_specifique=1257.4,
+        conductivite_thermique=0.076578,
+    )
+
+    air = Substance(
+        temperature=423,
+        densite=0.8345,
+        chaleur_specifique=1014.5,
+        conductivite_thermique=0.034425,
+    )
+
+    subst_garnissage = Substance(
+        temperature=500,
+        densite=2600,
+        chaleur_specifique=1000,
+        conductivite_thermique=2,
+    )
 
     # Créer le garnissage
     garni = Garnissage(
-        rho_cp_f=rho_cp_f,
-        rho_cp_eq=rho_cp_eq,
-        k_eq=k_eq,
+        substance=subst_garnissage,
         vitesse_fluide=vitesse_fluide,
+        ratio_volume_solide=0.5,
         longueur_garnissage=1.6,
-        temperature_initiale_garnissage=300,
-        resolution_temporelle=0.01,  # augmenter pour accélérer le calcul (et la vitesse de l'animation)
+        temperature_initiale_garnissage=450,
+        resolution_temporelle=0.3,  # augmenter pour accélérer le calcul (et la vitesse de l'animation)
         resolution_spatiale=0.015,  # augmenter pour accélérer la simulation (mais moins précis)
     )
 
     # Démarrer les phases
-    garni.demarrer_phase(temperature_fluide=500, direction=1, duree=2)
-    garni.demarrer_phase(temperature_fluide=100, direction=-1, duree=2)
+    for i in range(10):
+        print(f"Cycle {i+1}")
+        garni.demarrer_phase(air, direction=1, duree=100)
+        garni.demarrer_phase(air, direction=1, duree=100)
+        garni.demarrer_phase(co2, direction=-1, duree=100)
+        print(f"\n")
 
     # Animations
-    garni.animer_ligne("ligne")
-    garni.animer_heatmap("heatmap")
+    garni.animer_ligne("ligne", step=10)
+    garni.animer_heatmap("heatmap", step=10)
